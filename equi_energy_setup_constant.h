4d040ebc8626f28f535ffd01b88a51f251d591f5
Hongwei Wu <parb220@gmail.com>
2012-07-03 10:22:53 -0400

    minor changes to equi_energy_setup_constant.h

--- equi_energy_setup_constant.h
+++ equi_energy_setup_constant.h
@@ -4,13 +4,13 @@
 const double C = 1.4;
 const int NUMBER_ENERGY_LEVEL = 5;
 const int BURN_IN_PERIOD = 5000;
-const int BUILD_INITIAL_ENERGY_SET_PERIOD = 20000;
+const int BUILD_INITIAL_ENERGY_SET_PERIOD = 50000;
 const double H0 = 0.0;
 const double HK_1 = 63.2;
 const double T0 = 1.0;
 const double TK_1 = 60.0;
 const int DATA_DIMENSION = 2;
-const int SIMULATION_LENGTH = 1000000;
+const int SIMULATION_LENGTH = 5000000;
 const double PEE = 0.1;
 const int DEPOSIT_FREQUENCY = 5;
 #endif
